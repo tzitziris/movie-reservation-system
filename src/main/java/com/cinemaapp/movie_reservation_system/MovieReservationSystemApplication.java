9b77dd528f2a4cdfb93fe98aac20fa79cb6a3de8
tzitziris <stzitzires@gmail.com>
2025-09-11 23:54:17 +0300

    first commit

---
+++ src/main/java/com/cinemaapp/movie_reservation_system/MovieReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cinemaapp.movie_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinemaapp/movie_reservation_system/MovieReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemaapp.movie_reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
